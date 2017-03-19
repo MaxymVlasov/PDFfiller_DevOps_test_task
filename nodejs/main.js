bf1d305edc41fa357b280c1aea095c3067119a00
MaxymVlasov <m.vlasov@post.com>
2017-03-19 14:45:39 +0200

    Create routs and very basic html-pages
    All not-found routs redirect to root-route

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,7 +1,33 @@
 'use strict'
 
 const http = require('http')
+const url = require('url')
+const fs = require('fs')
+
+const routes = {
+  '': 'index.html',
+  index: 'index.html',
+  kill: 'kill.html',
+  status: 'status.html'
+}
+
+function givePage (req, res, route) {
+  fs.readFile(`${__dirname}/html/${route}`, (err, data) => {
+    if (err) throw err
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data, 'utf8')
+    res.end()
+  })
+}
+
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello Apache!\n')
+  // parse the pathname as a url, get the trimmed route from the request
+  const path = url.parse(req.url).pathname
+
+  const basePath = path.split('/')[1]
+  if (routes.hasOwnProperty(basePath)) {
+    givePage(req, res, routes[basePath])
+  } else {
+    givePage(req, res, routes[''])
+  }
 }).listen(8000, '0.0.0.0')
